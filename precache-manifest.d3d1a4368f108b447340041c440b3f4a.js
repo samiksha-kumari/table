4528d1405dff37a6ddee13151dad3f76d213ebc8
samiksha-kumari <samiksha-kumari_outlook.com>
2019-10-17 23:44:04 +0530

    Updates

---
+++ precache-manifest.d3d1a4368f108b447340041c440b3f4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631b5a7fa302e033520f132c36d06d75",
+    "url": "/table/index.html"
+  },
+  {
+    "revision": "f17a84e996bd60209888",
+    "url": "/table/static/css/main.8f664335.chunk.css"
+  },
+  {
+    "revision": "22b14cf520734ebb2a5a",
+    "url": "/table/static/js/2.a278b1f9.chunk.js"
+  },
+  {
+    "revision": "f17a84e996bd60209888",
+    "url": "/table/static/js/main.82f6238e.chunk.js"
+  },
+  {
+    "revision": "13efc87b2a3c8fc72d1e",
+    "url": "/table/static/js/runtime-main.4db02885.js"
+  }
+]);
